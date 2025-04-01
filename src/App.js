eb29859d2d9f5e60a1427308b867d16563e439a3
Lynn52 <139542759+Lynn52@users.noreply.github.com>
2025-04-02 04:31:19 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import {Route, Routes, Link} from 'react-router-dom';
+import {Route, Routes, Link, Router} from 'react-router-dom';
 import Main from './main/main';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='*' element={<Main />}></Route>
       </Routes>
-    </>
+    </Router>
   );
 }
 
