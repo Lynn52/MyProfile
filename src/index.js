5cf0c537baea4205d36c2a705cd2036e9b8e1303
Lynn52 <139542759+Lynn52@users.noreply.github.com>
2025-04-02 04:21:44 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import './App.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
